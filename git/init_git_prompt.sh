a0e985f53cb1139739072cb497da48a044fac55b
Johannes Battenberg <battenberg@rz.uni-frankfurt.de>
2015-06-18 14:05:25 +0200

    extracted git info so it can be used standalone

---
+++ git/init_git_prompt.sh
@@ -0,0 +1,19 @@
+# Initialize colors.
+autoload -U colors
+colors
+
+# Allow for functions in the prompt.
+
+# Autoload zsh functions.
+fpath=(~/.zsh/git/include $fpath)
+autoload -U ~/.zsh/git/include/*(:t)
+
+# Enable auto-execution of functions.
+typeset -ga preexec_functions
+typeset -ga precmd_functions
+typeset -ga chpwd_functions
+
+# Append git functions needed for prompt.
+preexec_functions+='preexec_update_git_vars'
+chpwd_functions+='update_current_git_vars'
+precmd_functions+='precmd_update_git_vars'
