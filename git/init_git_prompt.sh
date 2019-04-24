59bd15cf1684eb9d57b2804013d12980faaa3fa1
Johannes Battenberg <battenberg@rz.uni-frankfurt.de>
2019-04-24 15:22:07 +0200

    refactoring and cleanup

--- git/init_git_prompt.sh
+++ git/init_git_prompt.sh
@@ -12,10 +12,41 @@ typeset -gA ZSH_GIT_COLORS
 : ${ZSH_GIT_COLORS[added]:="green"}
 : ${ZSH_GIT_COLORS[conflict]:="magenta"}
 : ${ZSH_GIT_COLORS[deleted]:="red"}
+: ${ZSH_GIT_COLORS[deleted]:="diverged"}
 : ${ZSH_GIT_COLORS[copied]:="yellow"}
 : ${ZSH_GIT_COLORS[renamed]:="yellow"}
 : ${ZSH_GIT_COLORS[untracked]:="red"}
 : ${ZSH_GIT_COLORS[stashed]:="white"}
+
+# define default symbols
+typeset -gA ZSH_GIT_SYMBOLS
+: ${ZSH_GIT_SYMBOLS[modified]:="M"}
+: ${ZSH_GIT_SYMBOLS[deleted]:="-"}
+: ${ZSH_GIT_SYMBOLS[added]:="+"}
+: ${ZSH_GIT_SYMBOLS[renamed]:="↹"}
+: ${ZSH_GIT_SYMBOLS[copied]:="↷"}
+: ${ZSH_GIT_SYMBOLS[untracked]:="?"}
+: ${ZSH_GIT_SYMBOLS[conflict]:="⚡"}
+: ${ZSH_GIT_SYMBOLS[stashed]:="S"}
+: ${ZSH_GIT_SYMBOLS[diverged]:="↕"}
+: ${ZSH_GIT_SYMBOLS[ahead]:="↑"}
+: ${ZSH_GIT_SYMBOLS[behind]:="↓"}
+: ${ZSH_GIT_SYMBOLS[ahead_behind]:="⇅"}
+
+__zsh_symbol_color_helper(){
+    echo '%F{'"$ZSH_GIT_COLORS[$1]"'}'"$ZSH_GIT_SYMBOLS[$1]"
+}
+
+# default state strings combined from symbol and color
+typeset -gA ZSH_GIT_STATE_STRINGS
+: ${ZSH_GIT_STATE_STRINGS[M]:="$(__zsh_symbol_color_helper modified)"}
+: ${ZSH_GIT_STATE_STRINGS[D]:="$(__zsh_symbol_color_helper deleted)"}
+: ${ZSH_GIT_STATE_STRINGS[RM]:="$(__zsh_symbol_color_helper renamed)"}
+: ${ZSH_GIT_STATE_STRINGS[A]:="$(__zsh_symbol_color_helper added)"}
+: ${ZSH_GIT_STATE_STRINGS[C]:="$(__zsh_symbol_color_helper copied)"}
+: ${ZSH_GIT_STATE_STRINGS[??]:="$(__zsh_symbol_color_helper untracked)"}
+: ${ZSH_GIT_STATE_STRINGS[UU]:="$(__zsh_symbol_color_helper conflict)"}
+
 # Allow for functions in the prompt.
 
 # Autoload zsh functions.
